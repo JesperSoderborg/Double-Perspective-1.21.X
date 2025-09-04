41aca2e1997f358733b2a55108d49898b66c82b8
JesperSoderborg <j.soderborg@grabo.org>
2025-09-04 21:56:54 +0200

    Working rectangles

--- src/main/java/net/laisvall/doubleperspective/client/WaypointRenderer.java
+++ src/main/java/net/laisvall/doubleperspective/client/WaypointRenderer.java
@@ -1,15 +1,22 @@
 package net.laisvall.doubleperspective.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RotationAxis;
 import net.minecraft.util.math.Vec3d;
+import org.joml.Matrix4f;
 import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
+import java.util.logging.Logger;
+
+import static net.laisvall.doubleperspective.DoublePerspective.LOGGER;
+import static net.minecraft.client.render.VertexFormats.POSITION_COLOR;
 
 public class WaypointRenderer {
     public static void init() {
@@ -22,12 +29,18 @@ public class WaypointRenderer {
 
         matrices.push();
 
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
         Vec3d camPos = camera.getPos();
         matrices.translate(-camPos.x, -camPos.y, -camPos.z);
 
         drawText("Hello Billboard1!", 0, 100, 0, 0.02f, 0xFFFFFF, true, matrices, vertexConsumers, camera);
-        drawText("Hello Billboard2!", 0, 103, 0, 0.005f, 0xFF0000, true, matrices, vertexConsumers, camera);
+        drawText("Hello Billboard2!", 0, 103, 0, 0.5f, 0xFF0000, true, matrices, vertexConsumers, camera);
         drawText("Hello Billboard3!", 0, 106, 0, 0.1f, 0x00FFFF, false, matrices, vertexConsumers, camera);
+
+        renderRectangle(matrices, 0, 90, 0, 4, 1, 0xFF0000FF);
+        renderRectangle(matrices, 0, 95, 0, 2, 3, 0xFFFFFFFF);
 
         matrices.pop();
     }
@@ -42,13 +55,19 @@ public class WaypointRenderer {
         matrices.translate(centerPos.x, centerPos.y, centerPos.z);
 
         // Rotate to face camera
-        Quaternionf camRotation = new Quaternionf()
-                .rotateY((float)Math.toRadians(-camera.getYaw()))
-                .rotateX((float)Math.toRadians(camera.getPitch()));
-        matrices.multiply(camRotation);
+
+        //        Quaternionf cameraRotation = new Quaternionf()
+        //                .rotateY((float) Math.toRadians(-camera.getYaw()))
+        //                .rotateX((float) Math.toRadians(camera.getPitch()));
+        //
+        //        matrices.multiply(cameraRotation);
+
+        matrices.multiply(RotationAxis.NEGATIVE_Y.rotationDegrees(camera.getYaw()));
+        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(camera.getPitch()));
 
         // Scale text
-        matrices.scale(-scale, -scale, scale);
+        matrices.scale(-1,-1,1);
+        matrices.scale(scale, scale, scale);
 
         int textWidth = textRenderer.getWidth(text);
         float offsetX = -textWidth / 2f;
@@ -66,5 +85,32 @@ public class WaypointRenderer {
         );
 
         matrices.pop();
+    }
+
+    private static void renderRectangle(MatrixStack matrices, double x, double y, double z, float width, float height, int color) {
+        Matrix4f matrix = matrices.peek().getPositionMatrix();
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.QUADS, POSITION_COLOR);
+
+        buffer.vertex(matrix, (float)x, (float)y, (float)z).color(color);
+        buffer.vertex(matrix, (float)(x+width), (float)y, (float)z).color(color);
+        buffer.vertex(matrix, (float)(x+width), (float)(y+height), (float)z).color(color);
+        buffer.vertex(matrix, (float)x, (float)(y+height), (float)z).color(color);
+
+        BufferRenderer.drawWithGlobalProgram(buffer.end());
+    }
+
+    private static void renderRomb(MatrixStack matrices, VertexConsumerProvider vertexConsumers) {
+        Matrix4f matrix = matrices.peek().getPositionMatrix();
+        Tessellator tessellator = Tessellator.getInstance();
+
+        BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.TRIANGLE_STRIP, POSITION_COLOR);
+
+        buffer.vertex(matrix, 20, 20, 5).color(0xFF0000FF).notify();
+        buffer.vertex(matrix, 5, 40, 5).color(0xFF0000FF);
+        buffer.vertex(matrix, 35, 40, 5).color(0x00FF00FF);
+        buffer.vertex(matrix, 20, 60, 5).color(0xFF00FFFF);
+
+        BufferRenderer.drawWithGlobalProgram(buffer.end());
     }
 }
